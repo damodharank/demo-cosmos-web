48bb8175117818fe89f23faa200489f6a81932e1
Damodharan <damodharan.kothanadaraman@capgemini.com>
2018-05-09 20:00:38 +0530

    with Cosmos Db Functions

--- index.js
+++ index.js
@@ -1,6 +1,120 @@
 var http = require('http');
 var url = require('url')
 
+var documentClient = require("documentdb").DocumentClient;
+var config = require("./config");
+
+
+
+var client = new documentClient(config.endpoint, { "masterKey": config.primaryKey });
+var HttpStatusCodes = { NOTFOUND: 404 };
+var databaseUrl = `dbs/${config.database.id}`;
+var collectionUrl = `${databaseUrl}/colls/${config.collection.id}`;
+
+
+/**
+ * Get the database by ID, or create if it doesn't exist.
+ * @param {string} database - The database to get or create
+ */
+function getDatabase() {
+    console.log(`Getting database:\n${config.database.id}\n`);
+    return new Promise((resolve, reject) => {
+        client.readDatabase(databaseUrl, (err, result) => {
+            if (err) {
+                if (err.code == HttpStatusCodes.NOTFOUND) {
+                    client.createDatabase(config.database, (err, created) => {
+                        if (err) reject(err)
+                        else resolve(created);
+                    });
+                } else {
+                    reject(err);
+                }
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+/**
+ * Get the collection by ID, or create if it doesn't exist.
+ */
+function getCollection() {
+    console.log(`Getting collection:\n${config.collection.id}\n`);
+
+    return new Promise((resolve, reject) => {
+        client.readCollection(collectionUrl, (err, result) => {
+            if (err) {
+                if (err.code == HttpStatusCodes.NOTFOUND) {
+                    client.createCollection(databaseUrl, config.collection, { offerThroughput: 400 }, (err, created) => {
+                        if (err) reject(err)
+                        else resolve(created);
+                    });
+                } else {
+                    reject(err);
+                }
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+
+
+/**
+ * Query the collection using SQL
+ */
+function queryCollection(response) {
+    console.log(`Querying collection through index:\n${config.collection.id}`);
+
+    return new Promise((resolve, reject) => {
+        client.queryDocuments(
+            collectionUrl,
+            'SELECT * FROM c'
+        ).toArray((err, results) => {
+            if (err) reject(err)
+            else {
+                for (var queryResult of results) {
+                    let resultString = JSON.stringify(queryResult);
+                    console.log(`\tQuery returned ${resultString}`);
+                }
+                console.log();
+                resolve(results);
+            }
+        });
+    });
+};
+
+/**
+ * Exit the app with a prompt
+ * @param {message} message - The message to display
+ */
+function exit(message) {
+    console.log(message);
+    console.log('Press any key to exit');
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 0));
+}
+
+
+// getDatabase()
+//     .then(() => getCollection())
+//     .then(() => queryCollection())
+//     .then(() => console.log(" Delete area..."))
+//     .then(() => console.log("Cleanup area..."))
+//     .then(() => { exit(`Completed successfully`); })
+//     .catch((error) => { exit(`Completed with error ${JSON.stringify(error)}`) });
+
+
+
+
+
+
+
+
+
 var server = http.createServer(onRequest);
 
 var port = process.env.PORT || 1337;
@@ -52,8 +166,13 @@ function showfunc(response, pathName){
 }
 
 function getUserDetails(response){
-    response.writeHead(200, {'Content-Type' : 'text/html'})
-    response.write('WILL GET THE USER DETIALS ');
+    // response.writeHead(200, {'Content-Type' : 'text/html'})
+    // response.write('WILL GET THE USER DETIALS ');
+    getDatabase()
+    .then(() => getCollection())
+    .then(() => queryCollection(response))
+    .then(() => { exit(`Completed successfully`); })
+    .catch((error) => { exit(`Completed with error ${JSON.stringify(error)}`) });
     response.end();
 }
 
